b2dd353d2315a34eb87fd02ad777599cdce52fbe
Xinxin Wei <xinxinw@imaginet.com>
2021-06-29 18:13:49 -0500

    Complete!

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'task tracker';
+  title: string = 'Task tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value;
+    });
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggleTask');
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string): boolean {
+    return this.router.url === route;
   }
 }
